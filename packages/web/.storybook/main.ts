4aabf0ebc23726e95a587266a9c5b8e901de47e4
Emory Freitas <emory.rvf@gmail.com>
2026-03-28 16:01:09 +0000

    Add missing files

---
+++ packages/web/.storybook/main.ts
@@ -0,0 +1,11 @@
+import type { StorybookConfig } from '@storybook/nextjs-vite';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.stories.@(ts|tsx)'],
+  framework: {
+    name: '@storybook/nextjs-vite',
+    options: {},
+  },
+};
+
+export default config;
