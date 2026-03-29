03285721a57259f869fe0dbca7e3b22bae39d8e7
rivailJunior <rivail.rj@gmail.com>
2026-03-29 00:57:05 -0400

    feat: onboarding page

---
+++ packages/app/src/components/onboarding/index.ts
@@ -0,0 +1,4 @@
+export { PageIndicator } from './PageIndicator';
+export { OnboardingOrganize } from './OnboardingOrganize';
+export { OnboardingTeams } from './OnboardingTeams';
+export { OnboardingShare } from './OnboardingShare';
